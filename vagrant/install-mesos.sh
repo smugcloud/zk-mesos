db864386f9806c2fd81a3f34b711379430fd9629
Marco Massenzio <marco@mesosphere.io>
2015-11-02 22:47:18 -0800

    Updated Vagrantfile to use v2 syntax; updated installation scripts; re-configured Mesos to use the ZooKeeper installed on the Master node

--- vagrant/install-mesos.sh
+++ vagrant/install-mesos.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y -f openjdk-7-jre-headless libsvn1
-sudo apt-get install -y -f
-
 # Install mesos package from Mesosphere
 # See: http://mesosphere.com/downloads
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
@@ -13,16 +9,22 @@ CODENAME=$(lsb_release -cs)
 # Add the repository
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
   sudo tee /etc/apt/sources.list.d/mesosphere.list
-sudo apt-get -y update
 
-sudo apt-get -y install mesos
+sudo apt-get update && sudo apt-get -y upgrade
+
+sudo apt-get install -y -f openjdk-7-jre-headless libsvn1
+sudo apt-get install -y -f
+
+sudo apt-get install -y mesos
 
 sudo mkdir -p /var/local/mesos/logs/{master,agent}
 sudo mkdir -p /var/local/mesos/{master,agent}
 
 # The package install will configure a master/agent to start
-# at boot on each server and point to a non-existent ZooKeeper
-# We need to get rid of that:
+# at boot on each server: we need to get rid of that.
 if [[ -e "/etc/init/mesos-*" ]]; then
-    sudo mv /etc/init/mesos-* /var/local/mesos
+    sudo rm /etc/init/mesos-*
 fi
+
+sudo service mesos-master stop
+sudo service mesos-slave stop
